fe1cf6a5a5853254194df73af5bb56d6570badaa
Shahnoor <64207572+Shahnoor-C@users.noreply.github.com>
2021-03-13 18:02:26 -0500

    Class components - putting it together assignment

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import PersonCard from './components/PersonCard'
 function App() {
   return(
   <div className="App">
-    <PersonCard firstName = {"Shahnoor"} lastName="Chowdhury" age="27" hairColor = "Black"></PersonCard>
-    <PersonCard firstName = {"Tiara"} lastName="Harris" age="27" hairColor = "Black"></PersonCard>
-    <PersonCard firstName = {"Bruce"} lastName="Wen" age="28" hairColor = "Black"></PersonCard>
-
-    <PersonCard firstName = {"Paha"} lastName="Chowdhury" age="32" hairColor = "Black"></PersonCard>
-    <PersonCard firstName = {"Vasanthi"} lastName="Chowdhury" age="28" hairColor = "Black"></PersonCard>
+    <PersonCard firstName = {"Shahnoor"} lastName={"Chowdhury"} age={27} hairColor = {"Black"}>
+      <h1>this is also a child component!</h1>
+      <p>This is a child component</p>
+    </PersonCard>
+    <PersonCard firstName = {"Tiara"} lastName={"Harris"} age={27} hairColor = {"Black"}></PersonCard>
+    <PersonCard firstName = {"Bruce"} lastName={"Wen"} age={28} hairColor = {"Black"}></PersonCard>
+    <PersonCard firstName = {"Paha"} lastName={"Chowdhury"} age={32} hairColor = {"Black"}></PersonCard>
+    <PersonCard firstName = {"Vasanthi"} lastName={"Chowdhury"} age={28}hairColor = {"Black"}></PersonCard>
     
 
   </div>
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 
     
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ageCount: this.props.age
+        };
+    }
+    
     render() {
+        const buttonPress =()=>{
+            console.log(`You clicked the button for ${this.props.firstName} `)
+            alert("Yeeeeeah boooiii!!!")
+            alert(`You've added the age to ${this.props.firstName}!`)
+            this.setState({ageCount:this.state.ageCount+1})
+        }
+
+        
         return (
         <div>
+      
            <h2>{this.props.firstName}, {this.props.lastName}</h2>
-           <p>Age:{this.props.age}</p>
+           {this.props.children}
+           <p>Age:{this.state.ageCount}</p>
            <p>Hair Color:{this.props.hairColor}</p>
+           <button onClick = {buttonPress}>Birthday Button for {this.props.firstName} {this.props.lastName}!</button>
         </div>
 
       
